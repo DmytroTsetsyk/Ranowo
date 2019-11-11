8b81f6dda67a459bb18a0bccf4e4cc6206590d66
Dmytro Tsetsyk <merlinultra@gmail.com>
2019-11-11 22:01:23 +0200

    Added js logic

---
+++ scripts/main.js
@@ -0,0 +1,49 @@
+$( document).ready(function() {
+    InitMinus();
+    InitPlus()
+});
+
+function InitMinus() {
+    $( "img[data-type='minus']").click(function() {
+        var element = this;
+        var parent = element.closest("div");
+        $(parent).find("input");
+        var input = $(parent).find("input");
+        var productCount = Number(input.val()) -1;
+        if((Number(input.val()) -1) > 0){
+            input.val(productCount);  
+            inputAttrVal = input.attr("product-id");
+            SetValueByAttr(productCount, inputAttrVal);
+        }
+    });   
+}
+
+function InitPlus() {
+    $("img[data-type='plus']").click(function() {
+        var element = this;
+        var parent = element.closest("div");
+        $(parent).find("input");
+        var input = $(parent).find("input");
+        inputAttrVal = input.attr("product-id");
+        var productCount = Number(input.val()) + 1;
+        input.val(productCount);  
+        SetValueByAttr(productCount, inputAttrVal);
+    });   
+}
+
+function SetValueByAttr(value, attr) {
+    var elements = $("input[product-id='" + attr + "']");
+    elements.val(value);
+    CalculatePrice();
+}
+
+function CalculatePrice() {
+    var totalPrice = null;
+    var elements = $(".modal-product-block .input-number");
+    $(elements).each(function() {
+        elementPrice = $(this).attr("product-price");
+        totalPrice += Number($(this).val()) * elementPrice;
+    });
+
+    $(".total-price").text(totalPrice.toFixed(2));
+}
