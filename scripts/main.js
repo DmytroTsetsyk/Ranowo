8fa92483efb556de5021d8e29b9989241b723ed2
Dmytro Tsetsyk <merlinultra@gmail.com>
2019-11-19 20:36:06 +0200

    Added impruvments

--- scripts/main.js
+++ scripts/main.js
@@ -3,19 +3,53 @@ $( document).ready(function() {
     InitPlus();
     InitSecondStep();
     InitThirdStep();
+    InitBackSecondStep();
+    InitBackFirstStep();
+    ShowSubscrModal()
 
 });
 
+function ShowSubscrModal(){
+    $(".steps-block").waypoint(function() {
+        if(!(sessionStorage.getItem('showSubscribeModal'))){
+            sessionStorage.setItem("showSubscribeModal", "false");
+            $('#subscribe-modal').modal('show');
+        }
+    });
+}
+
+function InitBackSecondStep() {
+    $(".go-second-step").click(function() {
+        HideAllModals();
+        $('#info-form-modal').modal('show');
+    });  
+}
+
+function InitBackFirstStep() {
+    $(".go-first-step").click(function() {
+        HideAllModals();
+        $('#checkout-modal').modal('show');
+    });  
+}
+
 function InitSecondStep() {
     $(".second-step").click(function() {
+        HideAllModals();
         $('#info-form-modal').modal('show');
     });  
 }
 
 function InitThirdStep() {
     $(".third-step").click(function() {
+
         $('#third-modal').modal('show');
     });  
+}
+
+function HideAllModals(){
+    $('#info-form-modal').modal('hide');
+    $('#checkout-modal').modal('hide');
+    $('#third-modal').modal('hide');
 }
 
 function InitMinus() {
@@ -25,7 +59,7 @@ function InitMinus() {
         $(parent).find("input");
         var input = $(parent).find("input");
         var productCount = Number(input.val()) -1;
-        if((Number(input.val()) -1) > 0){
+        if((Number(input.val()) -1) >= 0){
             input.val(productCount);  
             inputAttrVal = input.attr("product-id");
             SetValueByAttr(productCount, inputAttrVal);
